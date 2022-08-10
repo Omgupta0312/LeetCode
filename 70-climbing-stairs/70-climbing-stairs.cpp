586b035da249ae96d20d14726fcfa2f11b4300bb
OM GUPTA <omgupta0312@gmail.com>
2022-08-10 22:58:59 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.67%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=1)
+            return n;
+        int a=0,b=1;
+        for(int i=2;i<=n+1;i++)
+        {
+            int c=a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+     
+    }
+};
