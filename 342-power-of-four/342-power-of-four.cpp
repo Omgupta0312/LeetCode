c2f400d8c1ef39b391f4c0206754ddd190c02189
OM GUPTA <omgupta0312@gmail.com>
2022-08-22 10:39:51 +0530

    Time: 3 ms (58.67%), Space: 5.9 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,17 +1,16 @@
+// Here, instead of using the for loop/recursion, I will use some popular built in functions of GCC:
+// __builtin_popcount(x): Return the number of ones in the binary representation of x
+// __builtin_ctz(x): Return the number of trailing zeroes in x.
+// Let's see how we can apply the above function into this problem:
+// Suppose that x is in the form 4^k
+// Then we can change x to 2^(2 * k)
+// It is well known in bit manipulation that 2^(2 * k) is equal to 1 << (2 * k)
+// That means __builtin_popcount(x) == 1 (Since x only has one bit that got left-shifted), and __builtin_ctz(x) % 2 == 0 (Since the number of trailing zeroes are 2 * k, which is an even number).
+
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n<0)
-            return false;
-        for(int i=0;i<n;i++)
-        {
-            long long int x = pow(4,i);
-            if(x==n)
-                return true;
-            if(x>n)
-                return false;
-                
-        }
-        return false;
+        return __builtin_popcount(n) == 1 && __builtin_ctz(n) % 2 == 0;
     }
+
 };
