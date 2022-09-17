70814f51037a00adf2615175a884f9a9c5bb2396
OM GUPTA <omgupta0312@gmail.com>
2022-09-17 21:16:21 +0530

    Time: 9 ms (89.56%), Space: 8.9 MB (59.40%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minDistance(string &s, string &t) {
+         int n=s.size(),m=t.size();
+         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+         for(int j=0;j<=t.size();j++)
+             dp[0][j]=j;
+         for(int i=0;i<=s.size();i++)
+             dp[i][0]=i;
+
+            for(int i=1;i<=n;i++)
+            {
+                for(int j=1;j<=m;j++)
+                {
+                    if(s[i-1]==t[j-1])
+                         dp[i][j]=dp[i-1][j-1];
+                    else
+                         dp[i][j]=1+min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]));
+                }
+            }
+            return dp[n][m];
+    
+    }
+};
