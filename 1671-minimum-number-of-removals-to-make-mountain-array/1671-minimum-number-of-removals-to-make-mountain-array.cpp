a1c57a8595ab5c34f19c3f4421a0c76c3abf313b
OM GUPTA <omgupta0312@gmail.com>
2022-11-17 10:36:46 +0530

    Time: 927 ms (5.07%), Space: 12.1 MB (57.77%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,84 +1,84 @@
 class Solution {
 public:
-     vector<int> LISlefttoright(vector<int>&nums){
-        vector<int>dp(nums.size(),1);
-        for(int i =1;i<nums.size();i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j] && dp[i]<dp[j]+1){
-                    dp[i]=dp[j]+1;
+//      vector<int> LISlefttoright(vector<int>&nums){
+//         vector<int>dp(nums.size(),1);
+//         for(int i =1;i<nums.size();i++){
+//             for(int j=0;j<i;j++){
+//                 if(nums[i]>nums[j] && dp[i]<dp[j]+1){
+//                     dp[i]=dp[j]+1;
                    
-                }
+//                 }
                     
-            }
-        }
-        return dp;
+//             }
+//         }
+//         return dp;
     
-    }
-    vector<int> LISrighttoleft(vector<int>&nums){
+//     }
+//     vector<int> LISrighttoleft(vector<int>&nums){
        
-        vector<int>dp(nums.size(),1);
-        for(int i=nums.size()-2;i>=0;i--){
-            for(int j=nums.size()-1;j>i;j--){
-                if(nums[i]>nums[j] && dp[i]<dp[j]+1){
-                    dp[i]=dp[j]+1;
+//         vector<int>dp(nums.size(),1);
+//         for(int i=nums.size()-2;i>=0;i--){
+//             for(int j=nums.size()-1;j>i;j--){
+//                 if(nums[i]>nums[j] && dp[i]<dp[j]+1){
+//                     dp[i]=dp[j]+1;
+//                 }
+//             }
+//         }
+//         return dp;
+//     }
+//     int minimumMountainRemovals(vector<int>& nums) {
+//        vector<int>dp1=LISlefttoright(nums);
+//         vector<int>dp2=LISrighttoleft(nums);
+//         int result=-1;
+//         for(int i =0;i<nums.size();i++){
+          
+//             if(dp1[i]==1 || dp2[i]==1) continue; 
+            
+//             result=max(result,dp1[i]+dp2[i]-1); 
+//         }
+//         return nums.size()-result;
+//     }
+    vector<int> LIS_L(vector<int> &nums){
+        vector<int> dp(nums.size(),1);
+        for(int i=0;i<nums.size();i++){
+           for(int prev = 0 ;prev <i;prev++){
+                if(nums[prev] < nums[i] && 1+ dp[prev] > dp[i]){
+                    dp[i] = 1 +dp[prev];
                 }
             }
+        
         }
         return dp;
+      
+            
     }
+    vector<int> LIS_R(vector<int> &nums){
+        vector<int> dp(nums.size(),1);
+        for(int i=nums.size()-1;i>=0;i--){
+           for(int prev = nums.size()-1 ;prev > i;prev--){
+                if(nums[prev] < nums[i] && 1+ dp[prev] > dp[i]){
+                    dp[i] = 1 +dp[prev];
+                }
+            }
+        
+        }
+        return dp;
+      
+            
+    }
+    
     int minimumMountainRemovals(vector<int>& nums) {
-       vector<int>dp1=LISlefttoright(nums);
-        vector<int>dp2=LISrighttoleft(nums);
+        vector<int> dp1 = LIS_L(nums);
+        vector<int> dp2 = LIS_R(nums);
+       
+        
         int result=-1;
         for(int i =0;i<nums.size();i++){
             cout<<dp1[i]<<" "<<dp2[i]<<endl;
             if(dp1[i]==1 || dp2[i]==1) continue; 
-            
             result=max(result,dp1[i]+dp2[i]-1); 
         }
         return nums.size()-result;
+        
     }
-//     vector<int> LIS_L(vector<int> &nums){
-//         vector<int> dp(nums.size(),1);
-//         for(int i=0;i<nums.size();i++){
-//            for(int prev = 0 ;prev <i;prev++){
-//                 if(nums[prev] < nums[i] && 1+ dp[prev] > dp[i]){
-//                     dp[i] = 1 +dp[prev];
-//                 }
-//             }
-        
-//         }
-//         return dp;
-      
-            
-//     }
-//     vector<int> LIS_R(vector<int> &nums){
-//         vector<int> dp(nums.size(),1);
-//         for(int i=0;i<nums.size();i++){
-//            for(int prev = nums.size()-1 ;prev > i;prev--){
-//                 if(nums[prev] < nums[i] && 1+ dp[prev] > dp[i]){
-//                     dp[i] = 1 +dp[prev];
-//                 }
-//             }
-        
-//         }
-//         return dp;
-      
-            
-//     }
-    
-//     int minimumMountainRemovals(vector<int>& nums) {
-//         vector<int> dp1 = LIS_L(nums);
-//         vector<int> dp2 = LIS_R(nums);
-       
-        
-//         int result=-1;
-//         for(int i =0;i<nums.size();i++){
-//             cout<<dp1[i]<<" "<<dp2[i]<<endl;
-//             if(dp1[i]==1 || dp2[i]==1) continue; 
-//             result=max(result,dp1[i]+dp2[i]-1); 
-//         }
-//         return nums.size()-result;
-        
-    // }
 };
