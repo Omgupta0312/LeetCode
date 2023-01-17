de80144fbb733541d05a27f06497bcf163ce0214
OM GUPTA <omgupta0312@gmail.com>
2023-01-17 11:49:29 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,36 +3,115 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class DisjointSet {
+    vector<int> rank, parent, size; 
+public: 
+    DisjointSet(int n) {
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+        }
+    }
+
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findUPar(parent[node]); 
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+        }
+        else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        }
+        else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+}; 
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        // code here
-        priority_queue<pair<int,int>,vector<pair<int,int>> , greater<pair<int,int>>> pq;
-        pq.push({0,0});
-        
-        int sum=0;
-        vector<int> vis(V,0);
-        while(pq.empty()==false){
-            int node = pq.top().second;
-            int w = pq.top().first;
-            pq.pop();
-            
-            if(vis[node]) continue;
-            sum+=w;
-            vis[node]=1;
-            for(auto &it:adj[node]){
-                int adjNode = it[0];
-                int ediff = it[1];
-                if(vis[adjNode]==0){
-                   pq.push({ediff,adjNode});
-                }
+        //KRUSHKAL ALGORITHM:
+        vector<pair<int,pair<int,int>>> edges;
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                int adjNode=it[0];
+                int wt=it[1];
+                int node=i;
+                
+                edges.push_back({wt,{node,adjNode}});
             }
-            
         }
-        return sum;
+        DisjointSet ds(V);
+        sort(edges.begin(),edges.end());
+        int mst=0;
+        for(auto it:edges){
+            int wt = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+            if(ds.findUPar(u)!=ds.findUPar(v)){
+                mst+=wt;
+                ds.unionBySize(u,v);
+            }
+        }
+        return mst;
+        
+        
+        
+        
+        
+        // PRIMS ALGORITHM
+        // priority_queue<pair<int,int>,vector<pair<int,int>> , greater<pair<int,int>>> pq;
+        // pq.push({0,0});
+        
+        // int sum=0;
+        // vector<int> vis(V,0);
+        // while(pq.empty()==false){
+        //     int node = pq.top().second;
+        //     int w = pq.top().first;
+        //     pq.pop();
+            
+        //     if(vis[node]) continue;
+        //     sum+=w;
+        //     vis[node]=1;
+        //     for(auto &it:adj[node]){
+        //         int adjNode = it[0];
+        //         int ediff = it[1];
+        //         if(vis[adjNode]==0){
+        //           pq.push({ediff,adjNode});
+        //         }
+        //     }
+            
+        // }
+        // return sum;
     }
 };
 
